e731ee1d0390d27a6f83b03ee571ab795ffcf6be
TranPhuocLoi <phuocloi2203@gmail.com>
2021-01-28 18:11:07 +0700

    fix

--- app/controllers/admin/movies_dashboard_controller.rb
+++ app/controllers/admin/movies_dashboard_controller.rb
@@ -4,13 +4,5 @@ module Admin
     def index
       @movies = Movie.all
     end
-    def new
-      @movie = Movie.new
-    end 
-    def create
-      @movies = Movie.create!(title:params[:title], description:params[:description], genres:params[:genres], original_language:params[:original_language])
-      redirect_to admin_root_path
-    end
-    
   end
 end
